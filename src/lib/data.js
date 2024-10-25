0dc08e2eb75959b0d55b4468cdd5d175887c75f7
coderdiaz <coderdiaz@gmail.com>
2024-10-25 14:07:07 -0600

    feat: added temporary data file

---
+++ src/lib/data.js
@@ -0,0 +1,8 @@
+// Export data object with songs and playlist properties
+// songs: array of songs
+// playlist: array of songs that are added to the playlist
+// Migrate the data from this file to use Supabase instead
+export const data = {
+  songs: [],
+  playlist: [],
+};
